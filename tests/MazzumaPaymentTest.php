9df3a8a9f2e2b88246960a0f0d61c2c742157b72
bboampong <baffouraduboampong@gmail.com>
2018-08-09 15:19:33 +0000

    Updated Readme

---
+++ tests/MazzumaPaymentTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BaffourAdu\mazzuma;
+
+class MazzumaPaymentTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
